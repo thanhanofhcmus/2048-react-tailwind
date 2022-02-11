2f4c654bce5a8f580284ff300e350c2d772068e4
Nguyen Thanh An <thanhan030301@gmail.com>
2022-02-11 15:36:13 +0700

    more clean up

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,7 @@ import {
 import useKeypress from "react-use-keypress";
 
 const App = () => {
-    // const [grid, setGrid] = useState(makeGrid(4));
-    const [grid, setGrid] = useState([
-        [0, 0, 0, 0],
-        [4, 2, 2, 0],
-        [8, 16, 32, 64],
-        [1024, 512, 256, 128],
-    ]);
+    const [grid, setGrid] = useState(makeGrid(4));
     const [lastGrid, setLastGrid] = useState(grid);
     const [score, setScore] = useState(0);
     const [won, setWon] = useState(false);
@@ -60,35 +54,38 @@ const App = () => {
     })
     return (
         <div>
-            <p className="text-3xl mb-3 px-3 py-2 font-bold text-center text-red-500 bg-gray-200 w-fit mx-auto rounded-md drop-shadow-sm">Score: {score}</p>
-            <div className="bg-gray-200 p-2 grid grid-cols-4 grid-rows-4 w-fit mx-auto shadow rounded-xl">
+            <p className="text-3xl mb-3 px-3 py-2
+            font-bold text-center text-red-500 bg-gray-200
+            w-fit mx-auto rounded-md drop-shadow-sm">
+                Score: {score}
+            </p>
+            <div className="bg-gray-200 p-2 grid grid-cols-4 grid-rows-4
+            w-fit mx-auto shadow rounded-xl">
                 {grid.flat().map((c, i) => <Title key={i} content={c} />)}
             </div>
             <br />
             {
                 won &&
-                <div className="text-xl m-3 p-3 bg-green-400 text-gray-100 rounded-xl drop-shadow-sm w-fit mx-auto">
+                <div className="text-xl m-3 p-3 bg-green-400 text-gray-100
+                rounded-xl drop-shadow-sm w-fit mx-auto">
                     <p>You reached 2048, you won!</p>
                     <p>You can continue playing to reach even higher number</p>
                 </div>
             }
             {
                 lost &&
-                <div className="text-xl m-3 p-3 bg-red-400 text-gray-100 rounded-xl drop-shadow-sm w-fit mx-auto">
+                <div className="text-xl m-3 p-3 bg-red-400 text-gray-100
+                rounded-xl drop-shadow-sm w-fit mx-auto">
                     <p>Cannot make any more move, game over!</p>
                     <p>Press <code>R</code> to restart or press <code>Z</code> to undo last move</p>
                 </div>
             }
-            <div className="text-xl m-3 p-3 bg-gray-200 rounded-xl drop-shadow-sm w-fit mx-auto">
+            <div className="text-xl m-3 p-3 bg-gray-200 rounded-xl
+            drop-shadow-sm w-fit mx-auto">
                 <p>Use <code>WASD</code> or <code>&uarr;&larr;&darr;&rarr;</code> to move titles.</p>
                 <p>Use <code>R</code> to reset.</p>
                 <p>Use <code>Z</code> to undo last move.</p>
             </div>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
         </div>
     );
 };
